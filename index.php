8ed607ac70b2d39954ed31d69c905fc537e13e48
sherlou77 <89011878+sherlou77@users.noreply.github.com>
2021-08-16 12:12:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+	<html>
+<head>
+    <title>Hacked By https://www.alminbarsport.com/public/upload/images/1546091013_cdnImage.jpeg</title>
+    <meta name="author" content="sherlouk"/>
+    <meta name="description" content="sherlouk alenezi"/>
+    <meta name="keywords" content="sherlouk alenezi"/>
+	<meta charset="UTF-8">
+    <meta http-equiv="cache-control" content="index,cache">
+    <meta http-equiv="pragma" content="index,cache">
+    <meta name="theme-color" content="black">
+    <meta property="og:title" content="Hacked By sherlouk alenezi"/>
+    <meta property="og:description" content="sherlouk"/>
+    <meta property="og:image" content="https://c.top4top.io/p_15741hw1y0.jpg"/>
+    <link rel="shortcut icon" href="https://e.top4top.io/p_15540apes0.png"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"> </script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Share+Tech+Mono" rel="stylesheet" type='text/css'> 
+    <div style="height: auto; min-height: 100%;">
+    <div style="text-align: center; width: 800px; margin-left: -400px; position: absolute; top: 16%; left: 50%;">
+    <body bgcolor=#141414>
+</head>
+<body>
+<iframe width="0" height="0" scrolling="no" frameborder="no" allow="autoplay" src="https://h.top4top.io/m_1698ayesx0.mp3"></iframe>
+<img  width="550" src="https://argaamplus.s3.amazonaws.com/fd039a60-98f9-48fc-a287-72885b1c220b.jpg">
+<br><br>
+<font face="Share Tech Mono" size="8" color="white">Hacked By sherlouk alenezi</font>
+<br>
+	<br><br><br>
+<font face="Share Tech Mono" size="5" color="white">"Normalization is a betrayal"</font><br>
+<font face="Share Tech Mono" size="5" color="white">"اولا انتو اول من طبعتو و احتفلتو ب مناسبات اليهود"</font><br>
+<font face="Share Tech Mono" size="5" color="white">"الي مافيه خير لاهله مافيه خير للناس"</font><br>
+<font face="Share Tech Mono" size="5" color="white">"؟ 😂 pegasus يعني التطبيع كان علشان"</font><br>
+<font face="Share Tech Mono" size="5" color="white">"احفاد القرده و الخنازير ما بينفعونكم"</font><br>
+
+
+<br><br><br>
+<font color="white" font face="Share Tech Mono">&#12300;<marquee width="50%"  scrollamount="5" size="4">b4x - root-f16 - black master - p0sitive1337 - win7 - redvirus0 - mq505 - max-hacker  
+</marquee><font color="white">&#12301;</font>
+</center><br><br>
+<font face="Share Tech Mono" size="4" color="white"><br>&copy; Copyright 2021><font size="4" color="red">./kwt</font><br>
+<br>
+<a href=https://www.instagram.com/q8defacer52><font color="white">-= CONTACT =-</a></font>
+</body>
+</html>
